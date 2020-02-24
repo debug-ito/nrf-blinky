4ecae04c5280ae907897a6730c79bc0579c2d656
Toshio Ito <debug.ito@gmail.com>
2020-02-24 12:05:54 +0900

    trying to use timer interrupt. not working.

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,14 @@ extern crate panic_halt; // you can put a breakpoint on `rust_begin_unwind` to c
 // extern crate panic_itm; // logs messages over ITM; requires ITM support
 // extern crate panic_semihosting; // logs messages to the host stderr; requires a debugger
 
+use core::sync::atomic::{AtomicU8, Ordering::Relaxed};
 use cortex_m::asm;
 use cortex_m::interrupt::{free, Mutex};
+use cortex_m::peripheral::NVIC;
 use cortex_m_rt::entry;
 
-use nrf52840_pac::interrupt;
+use nrf52840_pac::interrupt; // for [interrupt] attribute?
+
 use nrf52840_hal::target::{
     timer0, Peripherals, TIMER0 as TIMER0_t
 };
@@ -22,6 +25,7 @@ use embedded_hal::digital::v2::OutputPin;
 
 
 static EV_TIMER0: Mutex<Option<timer0::EVENTS_COMPARE>> = Mutex::new(None);
+static COUNTER: AtomicU8 = AtomicU8::new(0);
 
 fn delay(count: u16) {
     for _ in 0 .. count {
@@ -38,6 +42,8 @@ fn config_timer0(timer: &TIMER0_t, period_usec: u32) {
     }
     timer.shorts.write(|w| w.compare0_clear().enabled());
     timer.intenset.write(|w| w.compare0().set_bit());
+    // nvic.enable(nrf52840_hal::target::Interrupt::TIMER0);
+    unsafe { NVIC::unmask(nrf52840_hal::target::Interrupt::TIMER0); }
 }
 
 fn start_timer0(timer: &TIMER0_t) {
@@ -59,10 +65,21 @@ fn TIMER0() {
     free(|cs| {
         if let Some(ev) = EV_TIMER0.borrow(&cs) {
             if ev.read().events_compare().bit_is_set() {
+                COUNTER.fetch_add(1, Relaxed);
                 ev.write(|w| w.events_compare().clear_bit());
             }
         }
     });
+}
+
+fn wait_change_counter(prev: &mut u8) {
+    loop {
+        let current = COUNTER.load(Relaxed);
+        if current != *prev {
+            *prev = current;
+            return;
+        }
+    }
 }
 
 #[entry]
@@ -70,13 +87,16 @@ fn main() -> ! {
     let pers = Peripherals::take().unwrap();
     let p0 = pers.P0.split();
     let mut led = p0.p0_07.into_open_drain_output(Disconnect0Standard1, Low);
-    config_timer0(&pers.TIMER0, 2_000_000);
+    let mut prev_counter = 0;
+    config_timer0(&pers.TIMER0, 5_000_000);
     start_timer0(&pers.TIMER0);
     
     loop {
         led.set_high().unwrap();
-        wait_timer0(&pers.TIMER0);
+        // wait_timer0(&pers.TIMER0);
+        wait_change_counter(&mut prev_counter);
         led.set_low().unwrap();
-        wait_timer0(&pers.TIMER0);
+        // wait_timer0(&pers.TIMER0);
+        wait_change_counter(&mut prev_counter);
     }
 }
