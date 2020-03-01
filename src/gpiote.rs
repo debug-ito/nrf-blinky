a91135b5612192757f32be32024fc01ea99341e3
Toshio Ito <debug.ito@gmail.com>
2020-03-01 17:41:59 +0900

    gpiote: preliminary implementation of ChanIn

--- src/gpiote.rs
+++ src/gpiote.rs
@@ -2,44 +2,120 @@
 
 use core::cell::RefCell;
 use cortex_m::interrupt::Mutex;
+use cortex_m::interrupt as cinterrupt;
+use cortex_m::peripheral::NVIC;
 
 use nrf52840_hal::target::{
+    interrupt, Interrupt,
     GPIOTE as GPIOTE_t,
 };
 
-struct ChanUninit {
-    port: u8,
-    pin: u8
+use crate::util::get_from_mutex;
+
+
+pub struct ChanUninit {
+    chan: usize,
 }
 
-struct ChanIn {
-    port: u8,
-    pin: u8
+pub struct ChanIn {
+    chan: usize,
 }
 
-struct ChanOut {
-    port: u8,
-    pin: u8
+pub struct ChanOut {
+    chan: usize,
+}
+
+pub type InputHandler = fn() -> ();
+
+pub struct ConfigIn {
+    pub port: u8,
+    pub pin: u8,
+    pub handler: InputHandler,
 }
 
 const CHAN_NUM: usize = 8;
-type InputHandler = fn() -> ();
+
 static DEVICE: Mutex<RefCell<Option<GPIOTE_t>>> = Mutex::new(RefCell::new(None));
-static HANDLER: [Mutex<RefCell<Option<InputHandler>>>; CHAN_NUM] = [Mutex::new(RefCell::new(None)); CHAN_NUM];
+static HANDLER: [Mutex<RefCell<Option<InputHandler>>>; CHAN_NUM] =
+    //// This initialization cannot be used if the type doesn't have Copy trait.
+    // [Mutex::new(RefCell::new(None)); CHAN_NUM];
+    [
+        Mutex::new(RefCell::new(None)),
+        Mutex::new(RefCell::new(None)),
+        Mutex::new(RefCell::new(None)),
+        Mutex::new(RefCell::new(None)),
+        Mutex::new(RefCell::new(None)),
+        Mutex::new(RefCell::new(None)),
+        Mutex::new(RefCell::new(None)),
+        Mutex::new(RefCell::new(None)),
+    ];
 
-// Copy traitを実装していないとこのやり方の配列初期化ができない？
+
+pub fn to_channels(dev: GPIOTE_t) -> [ChanUninit; 8] {
+    cinterrupt::free(|cs| {
+        DEVICE.borrow(&cs).replace(Some(dev));
+    });
+    return [
+        ChanUninit { chan: 0 },
+        ChanUninit { chan: 1 },
+        ChanUninit { chan: 2 },
+        ChanUninit { chan: 3 },
+        ChanUninit { chan: 4 },
+        ChanUninit { chan: 5 },
+        ChanUninit { chan: 6 },
+        ChanUninit { chan: 7 },
+    ];
+}
 
 
-// fn to_channels(dev: GPIOTE_t) -> [ChanUninit; 8] {
-//     // TODO
-// }
+#[interrupt]
+fn GPIOTE() {
+    cinterrupt::free(|cs| {
+        if let Some(dev) = get_from_mutex(&DEVICE, &cs) {
+            for chan_index in 0 .. CHAN_NUM {
+                let ev = &dev.events_in[chan_index];
+                if ev.read().events_in().bit_is_set() {
+                    ev.write(|w| w.events_in().clear_bit());
+                    if let Some(handler) = get_from_mutex(&HANDLER[chan_index], &cs) {
+                        handler();
+                    }
+                }
+            }
+        }
+    });
+}
 
-// impl ChanUninit {
-//     fn config_input(self) -> ChanIn {
-//         // TODO
-//     }
-// 
-//     fn config_output(self) -> ChanOut {
-//         // TODO
-//     }
-// }
+impl ChanUninit {
+    pub fn into_input(self, config: ConfigIn) -> ChanIn {
+        cinterrupt::free(|cs| {
+            match get_from_mutex(&DEVICE, &cs) {
+                None => panic!("This should not happen."),
+                Some(ref dev) => self.config_input(dev, config),
+            }
+        });
+        return ChanIn { chan: self.chan };
+    }
+
+    fn config_input(&self, dev: &GPIOTE_t, config: ConfigIn) {
+        dev.config[self.chan].write(|w| {
+            unsafe {
+                return w.mode().event()
+                    .port().bit(config.port != 0)
+                    .psel().bits(config.pin)
+                    .polarity().hi_to_lo();
+            }
+        });
+        cinterrupt::free(|cs| {
+            dev.intenset.write(|w| {
+                w.in0().set()
+            });
+            HANDLER[self.chan].borrow(&cs).replace(Some(config.handler));
+            unsafe { NVIC::unmask(Interrupt::GPIOTE); }
+        });
+    }
+
+    // fn config_output(self) -> ChanOut {
+    //     // TODO
+    // }
+}
+
